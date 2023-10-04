8e2296a1482b18f63931c3dee84ab87a77058d51
doriankaleb12 <146804704+doriankaleb12@users.noreply.github.com>
2023-10-03 22:17:04 -0600

    Add files via upload

---
+++ SUMATORIA/SUMA DE 8 EN C#/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    const int NUM = 8;
+    static void Main()
+    {
+        int[] nums = new int[NUM];
+        int total = 0;
+        for (int i = 0; i < NUM; i++)
+        {
+            Console.Write("Por favor, introduzca el numero: ");
+
+            nums[i] = int.Parse(Console.ReadLine());
+            total += nums[i];
+
+        }
+
+        Console.WriteLine("El total de numeros es: " + total);
+    }
+
+    
+
+}
---
+++ SUMATORIA/SUMA DE C++/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+#include <iostream >
+using namespace std;
+const int NUM = 8;
+int main()
+{
+	int nums[NUM];
+	int total = 0;
+	for (int i = 0; i < NUM; i++)
+	{
+		cout << "Por favor, introduzca el numero";
+		cin >> nums[i];
+		total += nums[i];
+	}
+	cout << "El total de nimeros es" << total << endl;
+	return 0;
+}
